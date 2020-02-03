ee14a47cc3a9d5656c903beadc4a0d4cb9067b2d
YashShettigar <47048488+YashShettigar@users.noreply.github.com>
2020-02-04 01:43:23 +0530

    Added Student/Instructor Tab to the login and signup page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,7 @@ import Container from '@material-ui/core/Container'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Link from '@material-ui/core/Link';
+import {Tabs, Tab, Paper} from '@material-ui/core'
 
 import { history } from '../router'
 import { Form } from '../styles/login'
@@ -12,11 +13,27 @@ const Login = props => {
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
-    
+    const [userValue, setUserValue] = useState("Student")
+
+    const handleChange = (e, val) => {
+        setUserValue(val)
+    }
 
     return (
         <Container style={{display: 'flex',  height: '100vh', justifyContent: 'center', flexDirection: 'column'}}>
             <h1>Login</h1>
+            <Paper square style={{marginTop: "12px", width: '320px'}}>
+                <Tabs
+                    value = {userValue}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    onChange={handleChange}
+                    aria-label="disabled tabs example"
+                >
+                    <Tab label="Student" value="Student" />
+                    <Tab label="Instructor" value="Instructor" />
+                </Tabs>
+            </Paper>
             <Form>
             <TextField id="outlined-basic" label="Username *" variant="outlined" 
             value={username}
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -5,6 +5,7 @@ import Button from '@material-ui/core/Button'
 import Link from '@material-ui/core/Link'
 import { history } from '../router'
 import { Form } from '../styles/login'
+import {Paper, Tabs, Tab} from '@material-ui/core'
 
 
 const SignUp = props => {
@@ -12,11 +13,28 @@ const SignUp = props => {
     const [username, setUsername] = useState("")
     const [name, setName] = useState("")
     const [password, setPassword] = useState("")
+    const [userValue, setUserValue] = useState("Student")
 
+    const handleChange = (e, val) => {
+        setUserValue(val)
+    }
+    
     return (
         <Container style={{display: 'flex',  height: '100vh', justifyContent: 'center', flexDirection: 'column'}}>
             <h1>Sign Up</h1>
             <p>Join now to experience a revolutionized way of learning!</p>
+            <Paper square style={{marginTop: "12px", width: '320px'}}>
+                <Tabs
+                    value = {userValue}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    onChange={handleChange}
+                    aria-label="disabled tabs example"
+                >
+                    <Tab label="Student" value="Student" />
+                    <Tab label="Instructor" value="Instructor" />
+                </Tabs>
+            </Paper>
             <Form height="400px">
             <TextField id="outlined-basic" label="Name *" variant="outlined" 
             value={name}
