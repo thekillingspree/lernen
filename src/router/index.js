c4d1cc8e8cea531bf99589468954dd6d5f2e27ea
Ajesh DS <ajeshd17@gmail.com>
2020-02-03 18:15:34 +0530

    Theme, Login and Signup

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import { Router, Route, Switch} from 'react-router-dom';
 import {createBrowserHistory} from 'history';
 import Home from '../pages/Home'
 import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
 const history = createBrowserHistory();
 
 const AppRouter = () => (
     <Router history={history}>
         <Switch>
             <Route exact={true} path={"/"} component={Home} />
-            <Route exact={true} path={"/login"} component={Login} />
+            <Route path={"/login"} component={Login} />
+            <Route path={"/signup"} component={SignUp} />
         </Switch>
     </Router>
 );
--- src/styles/login.js
+++ src/styles/login.js
@@ -3,8 +3,7 @@ import styled from 'styled-components'
 export const Form = styled.form`
     display: flex;
     flex-direction: column;
+    min-height: ${props => props.height || '250px'};
+    justify-content: space-evenly;
     width: 100%;
-    & div {
-        margin: 10px 0;
-    }
 `
